0d5ac6ee9d294b3c7e5b38e7ebce6c4ec0ce228f
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-09-01 11:24:43 +0530

    back end data base connected and remove unnessary file

--- server/index.js
+++ server/index.js
@@ -2,3 +2,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://social-media:social-media@cluster0.pmw5g.mongodb.net/social-media?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running port is ${PORT}`)))
+    .catch((err) => console.log(err.message));
+
