82f747ca9b08eb407a62e0d8e0e37dc2ffc9f121
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-09-06 13:22:53 +0530

    delete post done

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -42,3 +42,15 @@ export const updatePost = async (req, res) => {
 
   res.json(updatePost);
 };
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send("No post with that Id");
+  }
+
+  await PostMessage.findByIdAndDelete(id);
+
+  res.json({ msg: "Post deleted Succefully" });
+};
